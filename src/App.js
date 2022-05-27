85a1b9ee9f1595e5274c94ff3d8253ea4c5e38eb
Gagandeep <gagandeep.wadhwa@gmail.com>
2022-05-27 18:58:13 +0530

    chore: test cases setup with layout component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Router } from '@reach/router';
+
+import Repositories from './pages/Repositories';
+import ThemeProvider from './components/ThemeProvider';
+import Body from './layout/Body';
+      
+const App = () => (
+  <ThemeProvider>
+    <Body>
+      <Router>
+        <Repositories path="/" />
+      </Router>
+    </Body>
+  </ThemeProvider>
+);
+
+export default App;
